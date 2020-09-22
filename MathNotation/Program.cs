5c8eddf99c6b8eca8b90c56aa93c83a39e74fde3
Insaf Developer <insaf.giniyatov@mail.ru>
2020-09-22 15:07:02 +0300

    перевод из 10 в k чную

---
+++ MathNotation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace MathNotation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("Введите число в десятичной системе и систему в которую хотите перевести");
+            WriteLine(Convert.ToString(Int32.Parse(ReadLine()), Int32.Parse(ReadLine())));
+        }
+    }
+
+    
+}
